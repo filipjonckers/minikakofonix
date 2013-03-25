0690e648de27213578257c015eb191d7c7040fce
Filip Jonckers <filip@jonckers.org>
2013-03-25 23:59:49 +0100

    minikakofonix v1.01 cleaned up code added command line arguments added block time logic

---
+++ src/be/belgocontrol/sur/minikakofonix/ShutdownThread.java
@@ -0,0 +1,27 @@
+package be.belgocontrol.sur.minikakofonix;
+
+/**
+ * ShutdownThread.
+ * 
+ * Copyright (c) 2013 Filip Jonckers.
+ * 
+ */
+
+public class ShutdownThread extends Thread
+{
+    private Minikakofonix kakofonix_ = null;
+
+    public ShutdownThread(Minikakofonix kakofonix)
+    {
+        super();
+        kakofonix_ = kakofonix;
+    }
+
+    /**
+     * Cleanly close the current recording.
+     */
+    public void run()
+    {
+        kakofonix_.stop();
+    }
+}
